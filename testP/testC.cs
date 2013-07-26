ef6be52ec07d8859465e0fdebf07c17acec4f738
marcoHub <you@example.com>
2013-07-26 10:47:54 -0400

    first commit

---
+++ testP/testC.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SilkTest.Ntf;
+
+namespace testN {
+
+	[TestClass]
+	public class testC {
+
+		private readonly Desktop _desktop = Agent.Desktop;
+
+		[TestInitialize]
+		public void Initialize() {
+			BaseState baseState = new BaseState("%ProgramFiles%\\Microsoft Office\\Office12\\OUTLOOK.EXE", "/Window[@caption='Outlook Today - Microsoft Outlook']");
+			baseState.WorkingDirectory = "C:\\Windows\\system32";
+			baseState.Execute();
+		}
+
+		[TestMethod]
+		public void outbox() {
+			Window outlookTodayMicrosoftOutlook = _desktop.Window("@caption='*Microsoft Outlook'");
+			//outlookTodayMicrosoftOutlook.Control("[@windowClassName='NetUIHWND'][1]").Click(MouseButton.Left, new Point(77, 139), ModifierKeys.None);
+			outlookTodayMicrosoftOutlook.Control("[@windowClassName='NetUIHWND'][1]").Click(MouseButton.Left);
+            //outlookTodayMicrosoftOutlook.Control("[*]").GetPropertyList()[1];
+
+            Console.WriteLine(outlookTodayMicrosoftOutlook.Control("@windowClassName='NetUIHWND'"));
+            //Console.WriteLine(outlookTodayMicrosoftOutlook.GetPropertyList()[8]);
+            //Console.WriteLine(outlookTodayMicrosoftOutlook.GetPropertyList()[9]);
+
+            //Window inboxMicrosoftOutlook = _desktop.Window("@caption='Inbox - Microsoft Outlook'");
+			//inboxMicrosoftOutlook.Control("[@windowClassName='NetUIHWND'][1]").Click(MouseButton.Left, new Point(78, 217), ModifierKeys.None);
+            //Console.ReadKey();
+		}
+
+	}
+
+}
